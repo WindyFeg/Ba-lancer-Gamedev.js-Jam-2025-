6c1270752ecec3dba051a3d8c85049bd5a760b1c
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2025-04-26 17:01:08 +0700

    player prefab

---
+++ Assets/Scripts/Player/PlayerAttackRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerAttackRange : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer attackRangeSprite;
+        private float _defaultScaleTransform;
+
+        private void Awake()
+        {
+            // Get the default scale of the sprite
+            _defaultScaleTransform = attackRangeSprite.transform.localScale.x;
+        }
+        
+        public void SetAttackRange(float range)
+        {
+            // Set the scale of the sprite to match the attack range
+            attackRangeSprite.transform.localScale = new Vector3(_defaultScaleTransform * range, _defaultScaleTransform * range, 1);
+        }
+    }
+}
